70c030db1879b4c8cfb2dd4320f60197842c11cf
taliev-1p <taliev-1p@users.noreply.github.com>
2021-04-03 00:21:54 +0300

    Create scala-hello.sh

---
+++ scala-hello.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+L=$(stat -f $0)
+L=$(dirname $L)/lib
+cp="$(echo ${L}/*.jar | sed 's/ /:/g')"
+
+exec scala -toolcp ${L} -deprecation -feature -savecompiled -classpath $cp $0 $0 $@
+
+exit
+!#
+
+// Say hello to the first argument 
+
+object HelloWorld {
+  def main(args: Array[String]) {
+    println("Hello, world! " + args.toList)
+  }
+}
